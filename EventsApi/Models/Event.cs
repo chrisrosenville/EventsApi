bb33373d3c5ff310bda13ed2db229124f1f3cb6c
chris <chris@TOWER>
2025-05-01 20:13:05 +0200

    Add project files.

---
+++ EventsApi/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿namespace EventsApi.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int AttendeesCount { get; set; }
+        // Constructor
+        public Event(int id, string name, string description, DateTime date, string location, int attendeesCount)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Date = date;
+            Location = location;
+            AttendeesCount = attendeesCount;
+        }
+    }
+}
